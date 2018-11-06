613663d03d5625264138d0da1995ac56787ddfdd
vsa@ambidexter.io <vsa@ambidexter.io>
2018-11-06 03:19:50 +0300

    ...

---
+++ Entity.cs
@@ -0,0 +1,38 @@
+﻿
+    public class Entity
+    {
+
+        public readonly string id;
+        public readonly List<IComponent> components;
+
+        public Entity()
+        {
+            id = Guid.NewGuid().ToString();
+            components = new List<IComponent>();
+        }
+
+        public Entity AddComponent(params IComponent[] component)
+        {
+            components.AddRange(component);
+            return this;
+        }
+
+        public Entity AddComponent(IComponent component)
+        {
+            components.Add(component);
+            return this;
+        }
+
+        public T GetComponent<T>() where T : IComponent
+        {
+            for (var index = 0; index < components.Count; index++)
+            {
+                var comp = components[index];
+                if (comp is T)
+                    return (T) comp;
+            }
+
+            return default(T);
+        }
+
+    }
